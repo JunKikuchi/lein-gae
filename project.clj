a8ac37bb85d25d82c993e3806900381328be5c04
Jun Kikuchi <kikuchi@bonnou.com>
2011-06-12 23:22:34 +0900

    Switched project group to clojars personal group

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-gae "1.0.0-SNAPSHOT"
-  :description "Leiningen plugin for Google App Engine."
+(defproject org.clojars.junkikuchi/lein-gae "0.0.1-SNAPSHOT"
+  :description "Leiningen plugin for Google App Engine (GAE)."
   :eval-in-leiningen true,
   :dependencies [[ring/ring-servlet "0.3.8"]])
